33c248333b6de8df3f0c5aa28c9ba008944257dd
Novaes, Laura G <laura.g.novaes@accenture.com>
2020-03-09 15:43:54 -0300

    implementation of the pages (search, comming-soon, configuration and recommendation) and modification in the model.ts nomination

---
+++ src/pages/comming-soon/comming-soon.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { CommingSoonPage } from './comming-soon';
+
+@NgModule({
+  declarations: [
+    CommingSoonPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(CommingSoonPage),
+  ],
+})
+export class CommingSoonPageModule {}
