22a21c00b876b1750a9cf5e1de6a399f71ec53c6
Novaes, Laura G <laura.g.novaes@accenture.com>
2020-03-10 15:16:04 -0300

    Criation of component and api request

---
+++ src/components/card-movie/card-movie.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+/**
+ * Generated class for the CardMovieComponent component.
+ *
+ * See https://angular.io/api/core/Component for more info on Angular
+ * Components.
+ */
+@Component({
+  selector: 'card-movie',
+  templateUrl: 'card-movie.html'
+})
+export class CardMovieComponent {
+
+  text: string;
+
+  constructor() {
+    console.log('Hello CardMovieComponent Component');
+    this.text = 'Hello World';
+  }
+
+}
