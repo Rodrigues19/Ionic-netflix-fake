5ce7e963a2916a1355d0f7d7507c837eb3b46619
Santana, Lais Souza d <lais.souza.d.santana@accenture.com>
2020-03-10 09:08:16 -0300

    Logon logic added login.ts

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,59 +1,49 @@
-import { Storage } from '@ionic/storage';
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { FormBuilder, FormGroup, Validators,FormControl } from '@angular/forms';
-import { ModelLogin } from '../../model/login.model';
-
+import { Storage } from "@ionic/storage";
+import { Component } from "@angular/core";
+import { IonicPage, NavController, NavParams } from "ionic-angular";
+import {FormBuilder,FormGroup,Validators, FormControl} from "@angular/forms";
+import { ModelLogin } from "../../model/login.model";
 
 @IonicPage()
 @Component({
-  selector: 'page-login',
-  templateUrl: 'login.html',
+  selector: "page-login",
+  templateUrl: "login.html"
 })
 export class LoginPage {
+  private form: FormGroup;
+  public usersRegistered: ModelLogin[] = [];
 
-  private form: FormGroup
-  public usersRegistered: ModelLogin[]=[]
-  public userRegister:ModelLogin
-
-  constructor(public navCtrl: NavController, public navParams: NavParams,public formBuilder:FormBuilder,private storage:Storage) {
-    this.form=formBuilder.group({
-      user: ['',[this.checkMail,Validators.required,Validators.pattern(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/)]],
-      password:['',[Validators.minLength(4),Validators.maxLength(60),Validators.required,]]
-    })
-   this.getUserRegistered()
+  constructor( public navCtrl: NavController,public navParams: NavParams,public formBuilder: FormBuilder,private storage: Storage) {
+    this.form = formBuilder.group({
+      user: ["",[this.checkMail,Validators.required,Validators.pattern(
+            /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/)]],
+      password: ["", [Validators.minLength(4), Validators.maxLength(60), Validators.required]]
+    });
+    this.getUserRegistered();
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad LoginPage');
+    console.log("ionViewDidLoad LoginPage");
   }
-  
-  checkMail(controls:FormControl):object{
-    if(controls.value.length < 10){
+
+  checkMail(controls: FormControl): object {
+    if (controls.value.length < 10) {
       return {
-        "samll":true
-        
-      }
+        samll: true
+      };
     }
     return null;
   }
 
-  public submit():void{
+  public submit(): void {
     this.usersRegistered.push(this.form.value);
     this.storage.set("usersRegistered", this.usersRegistered);
-    this.userRegister= this.form.get('user').value
-    console.log(this.userRegister)
-    if(this.usersRegistered.indexOf(this.userRegister)){
-      this.navCtrl.push('HomePage')
-    }
-    
+    this.navCtrl.push('HomePage')
   }
 
- private getUserRegistered(): any{
-    this.storage.get('usersRegistered').then((val)=> 
-    this.usersRegistered=val||[]
-  );
-  console.log(this.usersRegistered)
+  private getUserRegistered(): void {
+    this.storage
+      .get("usersRegistered")
+      .then(val => (this.usersRegistered = val || []));
   }
-
 }
