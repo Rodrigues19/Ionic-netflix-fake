e28e1b68bc7aac0d41b0973993a7a0a902db4870
Santana, Lais Souza d <lais.souza.d.santana@accenture.com>
2020-03-10 15:04:38 -0300

    Login.ts tested and revised

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -32,7 +32,6 @@ export class LoginPage {
           Validators.pattern(
             /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
           ),
-          Validators.minLength(10),
           Validators.required
         ]
       ],
