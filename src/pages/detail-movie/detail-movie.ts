c9d2ee9db8bf8e822fcbf5594b3dc10d52de1b53
Santana, Lais Souza d <lais.souza.d.santana@accenture.com>
2020-03-12 11:15:28 -0300

    Home and model editing

--- src/pages/detail-movie/detail-movie.ts
+++ src/pages/detail-movie/detail-movie.ts
@@ -1,4 +1,4 @@
-
+import { Storage } from '@ionic/storage';
 import { MovieModel } from './../../model/movie.model';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
@@ -10,14 +10,21 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 
 export class DetailMoviePage {
-  public add: boolean;
   public selectMovie:MovieModel= new MovieModel()
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  public myList:MovieModel[]=[]
+  constructor(public navCtrl: NavController, public navParams: NavParams,private storage:Storage) {
     this.selectMovie= navParams.get("movie");
   }
-  public addMyList():void{
-    this.add = !this.add;
-  } 
+
+  public async addMyList(movie:MovieModel):Promise<void> {
+    movie.add_myList=!movie.add_myList;
+    if(movie.add_myList){
+      this.myList.push(movie);
+     await this.storage.set('myList',this.myList);
+    }else{
+      this.myList= this.myList.filter(movie=>movie.add_myList);
+     await this.storage.set('myList',this.myList);
+    }
+  }
 
 }
