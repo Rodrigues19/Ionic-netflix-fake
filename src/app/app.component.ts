7290066cce3ad853664e904fd127923b03b29cf9
Novaes, Laura G <laura.g.novaes@accenture.com>
2020-03-12 12:13:49 -0300

    Creation of the avatar edition

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,18 +2,19 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
+import { App } from 'ionic-angular/components/app/app';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = "LoginPage";
+  rootPage: any = "LoginPage";
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
+      platform.registerBackButtonAction(() => {
+        // app.navPop();
+      });
       statusBar.styleDefault();
       splashScreen.hide();
     });
